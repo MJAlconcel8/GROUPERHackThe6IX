4669b0caf8961861c691308c6f438ba2d6aae886
Jason Yang <Jasonyang602@gmail.com>
2022-08-20 15:31:08 -0400

    init folders

---
+++ src/Pages/Landing.js
@@ -0,0 +1,20 @@
+
+import LandingCard from "../components/LandingCard.js";
+
+export default function Landing() {
+    
+    
+    return (
+        <>
+            <div className="header"></div>
+            <div className="logo"></div>
+            <h1 className="main-text">Your DREAM TEAM just one swipe away!</h1>
+            <div className="get-start-button">
+                <div className="get-started-button-content">
+                    <p className="get-start-but-text"></p>
+                    <img src="" alt="" className="arrow" />
+                </div>
+            </div>
+        </>
+    )
+}
