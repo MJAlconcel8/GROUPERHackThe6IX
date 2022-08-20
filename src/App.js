bdb8ca1a189afe241e9ef45813309671acec7097
Jason Yang <Jasonyang602@gmail.com>
2022-08-20 15:56:21 -0400

    jsx for landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
