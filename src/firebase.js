2397960d8eaf8ba3ae001b5878c6e0928b7fc9de
Jason Yang <Jasonyang602@gmail.com>
2022-08-20 19:05:35 -0400

    firebase init

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAMNxuTd3r3IyRyL3UZfn1ISS0tKeWs7kc",
+  authDomain: "grouper-a7d6d.firebaseapp.com",
+  projectId: "grouper-a7d6d",
+  storageBucket: "grouper-a7d6d.appspot.com",
+  messagingSenderId: "916416097895",
+  appId: "1:916416097895:web:5838bbc3ba57851f62aec2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
