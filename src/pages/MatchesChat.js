95cd069cfabaf9929bfdecc0c8b9b55b76781557
Jason Yang <Jasonyang602@gmail.com>
2022-08-21 08:01:55 -0400

    texting

--- src/pages/MatchesChat.js
+++ src/pages/MatchesChat.js
@@ -2,8 +2,16 @@ import Navbar from "../components/Navbar";
 import naomiwilliams from "../images/naomiwilliams.png";
 import backArrow from "../images/icon-park-outline_return.png";
 import chatBubble from "../images/chat_bubble.png"
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function MatchChat() {
+
+    const [ isTyping, setIsTyping ] = useState(false);
+
+    function handleSend() {
+        setIsTyping(true)
+    }
 
     return (
         <>
@@ -25,16 +33,17 @@ export default function MatchChat() {
                         <div className="other-chat-member">
                             <p className="chat-cta">Start chatting with</p>
                             <p className="matched-name-chat">Naomi Williams</p>
-                            <div className="matched-person-chat">
+                            {isTyping && <motion.div className="your-message" initial={{opacity: 0}} animate={{opacity: 1}}>Hey whatsup!!!</motion.div>}
+                            {isTyping && <motion.div className="matched-person-chat" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1}}>
                                 <img className="profile-pic" src={naomiwilliams} alt="Naomi Williams Profile Picture" />
                                 <img className="pending-chat-bubble" src={chatBubble} />
-                            </div>
+                            </motion.div>} 
                         </div>
                         <div className="user-chat-box">
                             <input className="mssg-box" type="text" placeholder="Say hello and introduce yourself!" />
-                            <div className="submit-message">
+                            <motion.div className="submit-message" whileHover={{ scale: 1.1 }} onClick={handleSend}>
                                 <p>Send</p>
-                            </div>
+                            </motion.div>
                         </div>
                     </div>
                 </div>
