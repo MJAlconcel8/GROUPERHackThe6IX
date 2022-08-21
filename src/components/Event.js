3abed94f07b96aa0dcae08a53ff720e536407583
Jason Yang <Jasonyang602@gmail.com>
2022-08-21 02:26:40 -0400

    props

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,9 +2,11 @@ import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
 
-export default function Event() {
+export default function Event(props) {
 
     const navigate = useNavigate();
+
+    
 
     function handleClick() {
         console.log("CLICK!");
@@ -16,12 +18,12 @@ export default function Event() {
 
             <div className="card-top">
                 <div className="card-titles">
-                    <h3 className="event-title">Hack The 6ix</h3>
-                    <h4 className="event-members">880 members</h4>
+                    <h3 className="event-title">{props.eventtitle}</h3>
+                    <h4 className="event-members">{props.members} members</h4>
                 </div>
                 <div className="event-logo" style={{ backgroundImage: `url("https://via.placeholder.com/500")` }} ></div>
             </div>
-            <div className="event-decription">Come join this fantastic 36 hour long event to test or aquire a new programming skill or framework in a motivating and focused setting.</div>
+            <div className="event-decription">{props.description}</div>
         </motion.div >
 
     )
