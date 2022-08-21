fad228b645772a05e1fde695552edb49fb82d138
Jason Yang <Jasonyang602@gmail.com>
2022-08-21 04:33:52 -0400

    asdfhg

--- src/components/Event.js
+++ src/components/Event.js
@@ -16,14 +16,14 @@ export default function Event(props) {
 
         <motion.div className="event-card" onClick={handleClick} whileHover={{ scale: 1.1 }}>
 
-            <div className="card-top">
+            <div className="card-top" style={{background: props.color}}>
                 <div className="card-titles">
                     <h3 className="event-title">{props.eventtitle}</h3>
                     <h4 className="event-members">{props.members} members</h4>
                 </div>
-                <div className="event-logo" style={{ backgroundImage: `url("https://via.placeholder.com/500")` }} ></div>
+                <img className="events-logo" src={props.logo} alt="" />
             </div>
-            <div className="event-description">Come join this fantastic 36 hour long event to test or acquire a new programming skill or framework in a motivating and focused setting.</div>
+            <div className="event-description">{props.description}</div>
         </motion.div >
 
     )
