108763dad2f5c00830a677fa87c89a3cb9206da9
Jason Yang <Jasonyang602@gmail.com>
2022-08-21 05:44:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Protected from "./components/Protected";
 import EventForm from "./pages/EventForm";
 import JoinEvent from "./pages/JoinEvent";
 import Hackpage from "./pages/HackThe6ix";
+import MatchChat from "./pages/MatchesChat";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/createevent" element={<EventForm />} />
             {/* <Route path="/joinevent" element={<JoinEvent />} /> */}
             <Route path="/Hackthe6ix" element={<Hackpage />} />
+            <Route path="/matcheschat" element={<MatchChat />} />
           </Routes>
         </AuthContextProvider >
       </BrowserRouter>
